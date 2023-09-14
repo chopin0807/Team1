d01e5296458ec9348c01fc2d365c5d5e67592f93
chopin0807 <chopin0807@naver.com>
2023-09-14 12:06:21 +0900

    오류부분 지우기

--- team.py
+++ team.py
@@ -1,8 +1,6 @@
 print("팀 프로젝트 프로젝트 테스트 파일입니다.")
-<<<<<<< HEAD
 print('효정이 수정함')
 print('dpdpdpd')
-=======
 
 # 평균을 구하는 함수
 def f_avg(data):
@@ -13,4 +11,3 @@ def f_avg(data):
 def f_sum(data):
     # 코드를 입력해주세요
     return 1
->>>>>>> edd2742e201e8e1e2346424aaaf1858897d818fb
