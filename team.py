fc548b3d4bf8a26bf75a5ade5fb6eb3685f7896e
hyojung <sonhj10318@gmail.com>
2023-09-14 12:17:19 +0900

    합계

--- team.py
+++ team.py
@@ -5,12 +5,11 @@ def f_avg(data): # 최준호 수정
 
 # 합계를 구하는 함수
 def f_sum(data):
-    # 코드를 입력해주세요
-    return 1
+    return sum(data)
 
 # 오름차순으로 정렬해주는 함수
 def f_sort(data) :
     return data.sort()
 
-# 평균구하기
+# 내림차순으로 정렬해주는 함수
 print(f_avg([1, 2, 3, 4, 5]))
